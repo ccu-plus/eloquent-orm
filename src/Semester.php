75a9ebb36658915fd9bb9dd24aeaa4de19aeca4d
BePsvPT <bepsvpt@protonmail.com>
2019-10-11 12:01:48 +0800

    add value attribute to semester model

--- src/Semester.php
+++ src/Semester.php
@@ -7,9 +7,28 @@ use Illuminate\Support\Str;
 /**
  * @property integer $id
  * @property string $name
+ * @property string|null $value
  */
 final class Semester extends Model
 {
+    /**
+     * 取得學期數字型態之值.
+     *
+     * @return string|null
+     */
+    public function getValueAttribute(): ?string
+    {
+        if (!$this->exists) {
+            return null;
+        }
+
+        return sprintf(
+            '%s%d',
+            mb_substr($this->name, 0, 3),
+            Str::endsWith($this->name, '上') ? 1 : 2
+        );
+    }
+
     /**
      * 取得最新學期 Eloquent Model.
      *
