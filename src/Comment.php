3e7cdaff3cd0b1140dc3476cacc7f06bf9043c27
BePsvPT <bepsvpt@protonmail.com>
2019-10-12 21:27:05 +0800

    add professor and comments relations to comment model

--- src/Comment.php
+++ src/Comment.php
@@ -3,19 +3,24 @@
 namespace CCUPLUS\EloquentORM;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property integer $id
  * @property integer $user_id
  * @property integer $comment_id
+ * @property integer $professor_id
  * @property string $content
  * @property boolean $anonymous
  * @property Carbon $created_at
  * @property Carbon $updated_at
  * @property Carbon|null $deleted_at
  * @property User $user
+ * @property Professor $professor
+ * @property Collection|Comment[] $comments
  */
 final class Comment extends Model
 {
@@ -47,5 +52,25 @@ final class Comment extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * 此評論評論教授.
+     *
+     * @return BelongsTo
+     */
+    public function professor(): BelongsTo
+    {
+        return $this->belongsTo(Professor::class);
+    }
+
+    /**
+     * 此評論之回覆.
+     *
+     * @return HasMany
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
